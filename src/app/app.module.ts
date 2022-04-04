a29de004310b244e0b2495e081407ec30f9870ee
Lucas Martín Mascheroni <lucasmasche@hotmail.com>
2022-04-03 22:29:44 -0300

    Clase 1 Ejercicio 2 Login esqueleto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Clase1Component } from './componentes/clase1/clase1.component';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Clase1Component
+    Clase1Component,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
