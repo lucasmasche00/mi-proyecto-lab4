f4751719c8da3d2ae23477d1d82201904a47176f
Lucas Martín Mascheroni <lucasmasche@hotmail.com>
2022-05-31 21:29:31 -0300

    Mayor o menor 1 a A

--- src/app/page/pipes/numero-carta.pipe.ts
+++ src/app/page/pipes/numero-carta.pipe.ts
@@ -8,6 +8,9 @@ export class NumeroCartaPipe implements PipeTransform {
   transform(value: number, ...args: any[]): any {
     let valueString = value.toString();
     switch (valueString) {
+      case '1':
+        valueString = 'A';
+        break;
       case '10':
         valueString = 'J';
         break;
